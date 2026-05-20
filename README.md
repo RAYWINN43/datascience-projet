833e3277b6de8bf10e250c24cfc7f2a049dec3ef
Antoine Gréa <112540434+aptitek@users.noreply.github.com>
2026-05-20 10:11:05 +0200

    refactor: new ci with release instead of commit

--- README.md
+++ README.md
@@ -306,10 +306,14 @@ Depuis la racine du projet, lancez :
   ```bash
   task clean
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> dc9bf4a (refactor: new ci with release instead of commit)
   ```
 
 ---
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 *Développé dans le cadre du projet fil rouge de Data Science.*
 =======
@@ -572,3 +576,6 @@ W10=
 >>>>>>> 917ae7d (Proper README + Clean)
 =======
 >>>>>>> ed5bf49 (Compile OJS+Plotly in gfm (github Markdown))
+=======
+*Développé dans le cadre du projet fil rouge de Data Science.*
+>>>>>>> dc9bf4a (refactor: new ci with release instead of commit)
--- tools/compile_ojs_in_markdown.py
+++ tools/compile_ojs_in_markdown.py
@@ -94,11 +94,16 @@ def compile_ojs_blocks(ojs_blocks):
 </script>
 """
 <<<<<<< HEAD
+<<<<<<< HEAD
     plotly_script = '<script src="https://cdn.plot.ly/plotly-2.35.2.min.js" charset="utf-8"></script>'
     return plotly_script + "\n" + html_input + "\n" + js_code
 =======
     return html_input + "\n" + js_code
 >>>>>>> ed5bf49 (Compile OJS+Plotly in gfm (github Markdown))
+=======
+    plotly_script = '<script src="https://cdn.plot.ly/plotly-2.35.2.min.js" charset="utf-8"></script>'
+    return plotly_script + "\n" + html_input + "\n" + js_code
+>>>>>>> dc9bf4a (refactor: new ci with release instead of commit)
 
 def main():
     if len(sys.argv) < 2:
@@ -126,6 +131,9 @@ def main():
     
     if compiled_widget:
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> dc9bf4a (refactor: new ci with release instead of commit)
         # 1. Translate echoed OJS blocks to standard JS codeblocks, and remove hidden ones
         def replace_block(match):
             block_content = match.group(1)
@@ -137,6 +145,7 @@ def main():
             return f"```javascript\n{clean_code}\n```"
 
         content = pattern.sub(replace_block, content)
+<<<<<<< HEAD
         
         # 2. Replace the <div id="dynamic-chart"...> element with the compiled interactive widget
         div_pattern = re.compile(r'<div\s+id="dynamic-chart".*?>\s*</div>', re.DOTALL)
@@ -146,18 +155,17 @@ def main():
         # Let's find the start of the first OJS block and the end of the chart div or the last block
         # To make it robust, we will replace the first OJS block with the widget,
         # and delete subsequent OJS blocks and the div id="dynamic-chart"
+=======
+>>>>>>> dc9bf4a (refactor: new ci with release instead of commit)
         
-        # Replace the first OJS block with the compiled widget
-        first_block_pattern = re.compile(r'```\s*\{\s*ojs\s*\}\n.*?\n```', re.DOTALL)
-        content = first_block_pattern.sub(compiled_widget, content, count=1)
-        
-        # Remove any other remaining OJS blocks
-        content = first_block_pattern.sub('', content)
-        
-        # Remove the <div id="dynamic-chart"...> element since the widget already has it
+        # 2. Replace the <div id="dynamic-chart"...> element with the compiled interactive widget
         div_pattern = re.compile(r'<div\s+id="dynamic-chart".*?>\s*</div>', re.DOTALL)
+<<<<<<< HEAD
         content = div_pattern.sub('', content)
 >>>>>>> ed5bf49 (Compile OJS+Plotly in gfm (github Markdown))
+=======
+        content = div_pattern.sub(compiled_widget, content)
+>>>>>>> dc9bf4a (refactor: new ci with release instead of commit)
         
         with open(md_path, 'w', encoding='utf-8') as f:
             f.write(content)
