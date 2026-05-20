ed8d55dfa41116c3c02e7973571841fbe61c556a
Antoine Gréa <112540434+aptitek@users.noreply.github.com>
2026-05-20 09:41:45 +0200

    Compile OJS+Plotly in gfm (github Markdown)

--- tools/compile_ojs_in_markdown.py
+++ tools/compile_ojs_in_markdown.py
@@ -93,8 +93,12 @@ def compile_ojs_blocks(ojs_blocks):
   }});
 </script>
 """
+<<<<<<< HEAD
     plotly_script = '<script src="https://cdn.plot.ly/plotly-2.35.2.min.js" charset="utf-8"></script>'
     return plotly_script + "\n" + html_input + "\n" + js_code
+=======
+    return html_input + "\n" + js_code
+>>>>>>> ed5bf49 (Compile OJS+Plotly in gfm (github Markdown))
 
 def main():
     if len(sys.argv) < 2:
@@ -121,6 +125,7 @@ def main():
     compiled_widget = compile_ojs_blocks(ojs_blocks)
     
     if compiled_widget:
+<<<<<<< HEAD
         # 1. Translate echoed OJS blocks to standard JS codeblocks, and remove hidden ones
         def replace_block(match):
             block_content = match.group(1)
@@ -136,6 +141,23 @@ def main():
         # 2. Replace the <div id="dynamic-chart"...> element with the compiled interactive widget
         div_pattern = re.compile(r'<div\s+id="dynamic-chart".*?>\s*</div>', re.DOTALL)
         content = div_pattern.sub(compiled_widget, content)
+=======
+        # We want to replace the sequence of OJS blocks and the dynamic chart div
+        # Let's find the start of the first OJS block and the end of the chart div or the last block
+        # To make it robust, we will replace the first OJS block with the widget,
+        # and delete subsequent OJS blocks and the div id="dynamic-chart"
+        
+        # Replace the first OJS block with the compiled widget
+        first_block_pattern = re.compile(r'```\s*\{\s*ojs\s*\}\n.*?\n```', re.DOTALL)
+        content = first_block_pattern.sub(compiled_widget, content, count=1)
+        
+        # Remove any other remaining OJS blocks
+        content = first_block_pattern.sub('', content)
+        
+        # Remove the <div id="dynamic-chart"...> element since the widget already has it
+        div_pattern = re.compile(r'<div\s+id="dynamic-chart".*?>\s*</div>', re.DOTALL)
+        content = div_pattern.sub('', content)
+>>>>>>> ed5bf49 (Compile OJS+Plotly in gfm (github Markdown))
         
         with open(md_path, 'w', encoding='utf-8') as f:
             f.write(content)
